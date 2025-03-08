d2be82c3c088f08c32ab6af7b8af91ce0f213073
Hirofumi Kurachi <158637673+HirofumiKurachi@users.noreply.github.com>
2025-03-08 21:20:21 +0900

    修正

--- js/main.js
+++ js/main.js
@@ -48,11 +48,9 @@ const swiper = new Swiper(".fv-swiper", {
       },
       // 768px以上の場合
       768: {
-<<<<<<< HEAD
+
         slidesPerView: 1.92, // 768px以上では2.1枚表示
-=======
-        slidesPerView: 2.1, // 768px以上では2.1枚表示
->>>>>>> 603bb78 (自分のコードを追加)
+
       }
     },
     autoplay: { // 自動再生
@@ -72,7 +70,7 @@ const swiper = new Swiper(".fv-swiper", {
   });
 
 
-<<<<<<< HEAD
+
   // マンスリープライススライダー-------------------------------------
   const mySwiper = new Swiper('.manthly-swiper', {
     loop: true,
@@ -84,16 +82,7 @@ const swiper = new Swiper(".fv-swiper", {
       // 768px未満の場合
       0: {
         slidesPerView: 1.22,
-=======
-  const mySwiper = new Swiper('.manthly-swiper', {
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 15,
-    breakpoints: {
-      // 768px未満の場合
-      0: {
-        slidesPerView: 1.2,
->>>>>>> 603bb78 (自分のコードを追加)
+
       },
       // 768px以上の場合
       768: {
@@ -118,11 +107,9 @@ const swiper = new Swiper(".fv-swiper", {
     breakpoints: {
       // 768px未満の場合
       0: {
-<<<<<<< HEAD
+
         slidesPerView: 1.28,
-=======
-        slidesPerView: 1.2,
->>>>>>> 603bb78 (自分のコードを追加)
+
       },
       // 768px以上の場合
       768: {
@@ -137,7 +124,7 @@ const swiper = new Swiper(".fv-swiper", {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     }
-<<<<<<< HEAD
+
   });
 
 
@@ -184,6 +171,4 @@ const swiper = new Swiper(".fv-swiper", {
 });
 
 
-=======
-  });
->>>>>>> 603bb78 (自分のコードを追加)
+
